017b6ac641f555bbac5ef141c255bd272c5dccc9
Pranav Konduru <konduru1976@mydusd.org>
2017-02-03 11:43:13 -0800

    Filling out the java class

--- Synthesizer_Version2/app/src/main/java/com/example/konduru91976/synthesizer_version2/SynthesizerActivity.java
+++ Synthesizer_Version2/app/src/main/java/com/example/konduru91976/synthesizer_version2/SynthesizerActivity.java
@@ -2,8 +2,12 @@ package com.example.konduru91976.synthesizer_version2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SynthesizerActivity extends AppCompatActivity {
+    private static final String TAG = SynthesizerActivity.class.getName();
+    private Button button1;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
