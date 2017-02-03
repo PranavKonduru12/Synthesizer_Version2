7699cc999c84eae8856a58878a0e4fff9255e473
Pranav Konduru <konduru1976@mydusd.org>
2017-02-03 11:14:56 -0800

    Test

---
+++ Synthesizer_Version2/app/src/main/java/com/example/konduru91976/synthesizer_version2/SynthesizerActivity.java
@@ -0,0 +1,13 @@
+package com.example.konduru91976.synthesizer_version2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SynthesizerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_synthesizer);
+    }
+}
